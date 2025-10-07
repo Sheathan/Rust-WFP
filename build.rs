4a20f3e520f5295d94db9d7fb833ffffc0e63720
Sheathan <quellodiabolico@gmail.com>
2025-10-06 23:18:06 -0400

    Create build.rs

---
+++ build.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let mut res = winres::WindowsResource::new();
+    res.set_manifest(r#"
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<assembly manifestVersion="1.0" xmlns="urn:schemas-microsoft-com:asm.v1"
+          xmlns:asmv3="urn:schemas-microsoft-com:asm.v3">
+  <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+    <security>
+      <requestedPrivileges>
+        <requestedExecutionLevel level="requireAdministrator" uiAccess="false"/>
+      </requestedPrivileges>
+    </security>
+  </trustInfo>
+</assembly>"#);
+    res.compile().unwrap();
+}
